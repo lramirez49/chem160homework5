3a503bcfdf93f09ea58fd08197071c6e0c3d6210
lramirez49 <54826114+lramirez49@users.noreply.github.com>
2019-10-06 23:39:10 -0700

    Add files via upload

---
+++ molemass.py
@@ -0,0 +1,8 @@
+names=["H", "C" , "N" , "O" , "P", "S"]
+masses=[1.00794,12.0107,14.0067,15,9994,30.973761,32.066]
+str1="1.00794"
+print(str1)
+str2='12.0107'
+print(str2)
+str4=str1+str2
+print(str4)
